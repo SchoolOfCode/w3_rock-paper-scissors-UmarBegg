efbd67e37b3ec3237e0d0c91a252d0d4ffaff383
Umar <umar97@hotmail.co.uk>
2021-05-04 19:36:27 +0100

    scoring added

--- main.js
+++ main.js
@@ -1,31 +1,37 @@
+let yes = true;
+let gamesPlayed = 0;
+let wins = 0;
+let losses = 0;
+let draws = 0;
+
 function getWinner(p1Move, p2Move) {
   switch (p1Move) {
     case "rock":
       switch (p2Move) {
         case "rock":
-          return 0;
+          return 0 + draws++;
         case "scissors":
-          return 1;
+          return 1 + wins++;
         case "paper":
-          return -1;
+          return -1 + losses++;
       }
     case "scissors":
       switch (p2Move) {
         case "rock":
-          return -1;
+          return -1 + losses++;
         case "scissors":
-          return 0;
+          return 0 + draws++;
         case "paper":
-          return 1;
+          return 1 + wins++;
       }
     case "paper":
       switch (p2Move) {
         case "rock":
-          return 1;
+          return 1 + wins++;
         case "scissors":
-          return -1;
+          return -1 + losses++;
         case "paper":
-          return 0;
+          return 0 + draws++;
       }
   }
 }
@@ -36,11 +42,14 @@ function generateComputerMove() {
   return moves[randomNumber];
 }
 
-let yes = true;
-
 while (yes === true) {
   let userMove = prompt("what are you?");
   let computerMove = generateComputerMove();
   console.log(getWinner(userMove, computerMove));
+  gamesPlayed++;
+  console.log("you have played: " + gamesPlayed + " games");
+  console.log("you have: " + wins + " wins");
+  console.log("you have: " + draws + " draws");
+  console.log("you have: " + losses + " losses");
   yes = confirm("Continue?");
 }
